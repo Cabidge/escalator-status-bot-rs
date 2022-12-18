6727856adf91b0dfffaa51b105c995362aaa7f23
Cabidge <cabbageboi69@gmail.com>
2022-12-18 18:38:38 -0500

    ?ghost change?

--- src/interaction.rs
+++ src/interaction.rs
@@ -1,5 +1,6 @@
-use crate::{data::status::Status, prelude::*};
+use crate::{data::status::Status, prelude::*, report_modal::ReportModal};
 
+use poise::Modal;
 pub use poise::serenity_prelude as serenity;
 
 const REPORT_OPEN_ID: &str = "BTN_REPORT_OPEN";
@@ -8,10 +9,34 @@ const REPORT_BLOCKED_ID: &str = "BTN_REPORT_BLOCKED";
 
 /// Handles an interaction created by a user.
 pub async fn handle_interaction(
-    _http: impl AsRef<serenity::Http>,
-    _interaction: &serenity::Interaction,
+    http: impl AsRef<serenity::Http>,
+    interaction: &serenity::Interaction,
     _data: &crate::Data,
 ) -> Result<(), Error> {
+    let Some(interaction) = interaction.to_owned().message_component() else { return Ok(()) };
+    let http = http.as_ref();
+
+    let _status = match interaction.data.custom_id.as_str() {
+        REPORT_OPEN_ID => Status::Open,
+        REPORT_DOWN_ID => Status::Down,
+        REPORT_BLOCKED_ID => Status::Blocked,
+        _ => return Ok(()),
+    };
+
+    interaction.create_followup_message(http, |msg| {
+        msg.content("Test")
+            .ephemeral(true)
+    }).await?;
+
+    let modal_id = format!("REPORT-MODAL-{}", interaction.id);
+
+    // override interaction response with the modal
+    let modal = ReportModal::create(None, modal_id);
+    interaction.create_interaction_response(http, |res| {
+        *res = modal;
+        res
+    }).await?;
+
     Ok(())
 }
 
