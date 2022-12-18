824fba6f5ab00763c78f08f38e910a12f3372960
Cabidge <cabbageboi69@gmail.com>
2022-12-17 20:24:06 -0500

    +comments

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,15 +1,18 @@
 use crate::prelude::*;
 
+/// Returns a vector containing all enabled bot commands.
 pub fn commands() -> Vec<poise::Command<crate::Data, Error>> {
     vec![register(), ping()]
 }
 
+/// Spawns a button panel to register application commands (dev-only).
 #[poise::command(prefix_command, owners_only)]
 async fn register(ctx: Context<'_>) -> Result<(), Error> {
     poise::builtins::register_application_commands_buttons(ctx).await?;
     Ok(())
 }
 
+/// Pings the bot for it to pong back.
 #[poise::command(slash_command, ephemeral = true)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Pong!").await?;
