1ba0fff62543e3897a1385d40ca6a8ed76d24178
Cabidge <cabbageboi69@gmail.com>
2022-12-19 18:41:08 -0500

    +gist command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -5,7 +5,7 @@ use crate::prelude::*;
 
 /// Returns a vector containing all enabled bot commands.
 pub fn commands() -> Vec<poise::Command<crate::Data, Error>> {
-    vec![register(), ping(), menu::menu(), history::history()]
+    vec![register(), ping(), menu::menu(), history::history(), gist()]
 }
 
 /// Spawns a button panel to register application commands (dev-only).
@@ -19,5 +19,15 @@ async fn register(ctx: Context<'_>) -> Result<(), Error> {
 #[poise::command(slash_command, ephemeral = true)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Pong!").await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, ephemeral = true)]
+async fn gist(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.defer_ephemeral().await?;
+
+    let gist = ctx.data().statuses.lock().await.gist();
+    ctx.send(move |msg| msg.embed(replace_builder_with(gist))).await?;
+
     Ok(())
 }
