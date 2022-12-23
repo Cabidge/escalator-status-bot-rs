f87e2f045df28e134c8acf36a548f2cb1e0f825c
Cabidge <cabbageboi69@gmail.com>
2022-12-23 15:09:20 -0500

    +redesigned alerts system with a button panel to add/remove from watch list

--- src/commands/alerts.rs
+++ src/commands/alerts.rs
@@ -1,68 +1,88 @@
+use std::time::Duration;
+
+use anyhow::anyhow;
 use itertools::Itertools;
 
-use crate::prelude::*;
+use crate::{
+    data::{ESCALATORS, ESCALATOR_COUNT, PAIR_ORDER},
+    prelude::*,
+};
 
-#[poise::command(slash_command, subcommands("add", "remove", "list"))]
+#[poise::command(slash_command, subcommands("edit", "list"))]
 pub async fn alerts(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-/// Add an escalator to your watch list and be alerted when it gets reported
-#[poise::command(slash_command, ephemeral = true)]
-pub async fn add(
-    ctx: Context<'_>,
-    #[description = "The beginning floor of the escalator"] start: u8,
-    #[description = "The ending floor of the escalator"] end: u8,
-    #[description = "Add both the up and down escalator"] pair: Option<bool>,
-) -> Result<(), Error> {
-    ctx.defer_ephemeral().await?;
-
-    if !is_valid_escalator(start, end) {
-        ctx.say(format!("The `{}-{}` is not a valid escalator.", start, end))
-            .await?;
-        return Ok(());
-    }
-
-    let mut alerts = ctx.data().alerts.lock().await;
-
-    alerts.add(ctx.author(), (start, end));
-
-    // add the inverse escalator if pair is true
-    if pair == Some(true) {
-        alerts.add(ctx.author(), (end, start));
-    }
-
-    ctx.say("Watch list updated.").await?;
-
-    Ok(())
+struct WatchListComponent {
+    watch_list: [bool; ESCALATOR_COUNT],
 }
 
-/// Remove an escalator from your watch list to no longer be alerted when it gets reported
+/// Edit your watch list and be alerted when any escalator on it gets reported.
 #[poise::command(slash_command, ephemeral = true)]
-pub async fn remove(
-    ctx: Context<'_>,
-    #[description = "The beginning floor of the escalator"] start: u8,
-    #[description = "The ending floor of the escalator"] end: u8,
-    #[description = "Add both the up and down escalator"] pair: Option<bool>,
-) -> Result<(), Error> {
+pub async fn edit(ctx: Context<'_>) -> Result<(), Error> {
     ctx.defer_ephemeral().await?;
 
-    if !is_valid_escalator(start, end) {
-        ctx.say(format!("The `{}-{}` is not a valid escalator.", start, end))
+    let watch_list = ctx
+        .data()
+        .alerts
+        .lock()
+        .await
+        .get_watch_list(ctx.author())
+        .unwrap_or_default(); // jesus why is this so long
+
+    let mut components = WatchListComponent { watch_list };
+
+    let handle = ctx
+        .send(|msg| msg.components(replace_builder_with(components.render())))
+        .await?;
+
+    let message = handle.message().await?;
+
+    let shard = &ctx.serenity_context().shard;
+    let res = loop {
+        let sleep = tokio::time::sleep(Duration::from_secs(2 * 60));
+        tokio::pin!(sleep);
+
+        let action = tokio::select! {
+            Some(action) = message.await_component_interaction(shard) => action,
+            _ = sleep => break Err(anyhow!("Timeout")),
+        };
+
+        action.defer(ctx).await?;
+
+        if let Some(list) = components.try_action(&action.data.custom_id) {
+            break Ok(list);
+        }
+
+        handle
+            .edit(ctx, |msg| {
+                msg.components(replace_builder_with(components.render()))
+            })
             .await?;
+    };
+
+    let Ok(watch_list) = res else {
+        handle.edit(ctx, |msg| {
+            msg.content("Interaction timed out, try again...")
+                .components(|components| {
+                    components.set_action_rows(vec![])
+                })
+        }).await?;
         return Ok(());
-    }
+    };
 
-    let mut alerts = ctx.data().alerts.lock().await;
+    ctx.data()
+        .alerts
+        .lock()
+        .await
+        .replace(ctx.author(), watch_list);
 
-    alerts.remove(ctx.author(), (start, end));
-
-    // remove the inverse escalator if pair is true
-    if pair == Some(true) {
-        alerts.remove(ctx.author(), (end, start));
-    }
-
-    ctx.say("Watch list updated.").await?;
+    handle
+        .edit(ctx, |msg| {
+            msg.content("Watch list updated.")
+                .components(|components| components.set_action_rows(vec![]))
+        })
+        .await?;
 
     Ok(())
 }
@@ -75,23 +95,22 @@ pub async fn list(ctx: Context<'_>) -> Result<(), Error> {
     let alerts = ctx.data().alerts.lock().await;
 
     let Some(watch_list) = alerts.get_watch_list(ctx.author()) else {
-        ctx.say("Your watch list is empty. Try adding escalators to it with the `/alert add <start> <end>` command.").await?;
+        ctx.say("Your watch list is empty. Try adding escalators to it with the `/alerts edit` command.").await?;
         return Ok(());
     };
 
     let statuses = ctx.data().statuses.lock().await;
     let message = String::from("**Your Watch List:**```\n")
         + &watch_list
-            .iter()
-            .sorted()
-            .filter_map(|escalator| {
-                let info = statuses.get_info(*escalator)?;
-                Some(format!(
-                    "{} {}-{}",
-                    info.status_emoji(),
-                    escalator.0,
-                    escalator.1
-                ))
+            .into_iter()
+            .enumerate()
+            .filter_map(|(index, selected)| {
+                if !selected {
+                    return None;
+                };
+                let escalator = ESCALATORS[index];
+                let emoji = statuses.get_info(escalator)?.status_emoji();
+                Some(format_escalator(emoji, escalator))
             })
             .join("\n")
         + "```";
@@ -99,4 +118,81 @@ pub async fn list(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say(message).await?;
 
     Ok(())
+}
+
+const ESCALATOR_BUTTON_ID_PREFIX: &str = "ALERTS-ESCALATOR-";
+const SUBMIT_BUTTON_ID: &str = "ALERTS-SUBMIT";
+const SUBMIT_BUTTON_EMOJI: char = '💾';
+
+impl WatchListComponent {
+    fn try_action(&mut self, action_id: &str) -> Option<[bool; ESCALATOR_COUNT]> {
+        if let Some(index) = action_id.strip_prefix(ESCALATOR_BUTTON_ID_PREFIX) {
+            self.toggle_index(index.parse().ok()?);
+            return None;
+        }
+
+        if action_id == SUBMIT_BUTTON_ID {
+            return Some(self.watch_list);
+        }
+
+        None
+    }
+
+    fn toggle_index(&mut self, index: usize) {
+        if index < ESCALATOR_COUNT {
+            self.watch_list[index] = !self.watch_list[index];
+        }
+    }
+
+    fn render(&self) -> serenity::CreateComponents {
+        let mut action_rows = PAIR_ORDER
+            .chunks(4)
+            .map(|row| {
+                let mut action_row = serenity::CreateActionRow::default();
+
+                for &index in row {
+                    action_row.create_button(|button| self.escalator_button(button, index));
+                }
+
+                action_row
+            })
+            .collect_vec();
+
+        action_rows.last_mut().unwrap().create_button(|button| {
+            button
+                .label("Save Watch List")
+                .custom_id(SUBMIT_BUTTON_ID)
+                .style(serenity::ButtonStyle::Success)
+                .emoji(SUBMIT_BUTTON_EMOJI)
+        });
+
+        let mut components = serenity::CreateComponents::default();
+
+        components.set_action_rows(action_rows);
+
+        components
+    }
+
+    fn escalator_button<'a>(
+        &self,
+        button: &'a mut serenity::CreateButton,
+        index: usize,
+    ) -> &'a mut serenity::CreateButton {
+        let (start, end) = ESCALATORS[index];
+        let label = format!("{}-{}", start, end);
+
+        let id = format!("{}{}", ESCALATOR_BUTTON_ID_PREFIX, index);
+
+        let style = if self.watch_list[index] {
+            serenity::ButtonStyle::Primary
+        } else {
+            serenity::ButtonStyle::Secondary
+        };
+
+        button.label(label).custom_id(id).style(style)
+    }
+}
+
+fn format_escalator(emoji: char, (start, end): Escalator) -> String {
+    format!("{} {}-{}", emoji, start, end)
 }
--- src/data/alerts.rs
+++ src/data/alerts.rs
@@ -3,24 +3,25 @@ use shuttle_persist::PersistInstance;
 
 use crate::prelude::*;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
-use super::{escalator_input::EscalatorInput, status::Status};
+use super::{escalator_input::EscalatorInput, status::Status, ESCALATORS, ESCALATOR_COUNT};
 
 #[derive(Debug)]
 pub struct Alerts {
-    watch_lists: HashMap<serenity::User, HashSet<Escalator>>,
+    watch_lists: HashMap<serenity::User, [bool; ESCALATOR_COUNT]>,
     should_save: bool,
 }
 
-type SimplifiedWatchlists = Vec<(u64, HashSet<Escalator>)>;
+type WatchList = [bool; ESCALATOR_COUNT];
+type SimplifiedWatchLists = Vec<(u64, WatchList)>;
 
 impl Alerts {
     pub async fn load_persist(
         persist: &PersistInstance,
         cache_http: impl serenity::CacheHttp,
     ) -> Self {
-        let Ok(simplified) = persist.load::<SimplifiedWatchlists>("alerts") else {
+        let Ok(simplified) = persist.load::<SimplifiedWatchLists>("alerts") else {
             return Alerts {
                 watch_lists: HashMap::new(),
                 should_save: true,
@@ -59,31 +60,36 @@ impl Alerts {
         persist.save("alerts", simplified).ok();
     }
 
-    pub fn add(&mut self, user: &serenity::User, escalator: Escalator) {
-        let watch_list = match self.watch_lists.get_mut(user) {
-            None => self.watch_lists.entry(user.to_owned()).or_default(),
-            Some(watch_list) => watch_list,
+    // Replace a user's original watch list with an updated list.
+    pub fn replace(&mut self, user: &serenity::User, watch_list: WatchList) {
+        // if the watch list is all unselected
+        if watch_list.iter().all(|selected| !selected) {
+            // remove the watch list
+            if self.watch_lists.remove(user).is_some() {
+                // if the user had a watch list originally, mark as should save
+                self.should_save = true;
+            }
+            return;
+        }
+
+        let Some(original_list) = self.watch_lists.get_mut(user) else {
+            // if there was no list before, add a watch list for the user
+            // and mark as should save
+            self.watch_lists.insert(user.to_owned(), watch_list);
+            self.should_save = true;
+            return;
         };
 
-        if watch_list.insert(escalator) {
+        // if the new watch list is different from the original
+        if original_list != &watch_list {
+            // replace the watch list and mark as should save
+            *original_list = watch_list;
             self.should_save = true;
         }
     }
 
-    pub fn remove(&mut self, user: &serenity::User, escalator: Escalator) {
-        let Some(list) = self.watch_lists.get_mut(user) else { return };
-        if list.remove(&escalator) {
-            self.should_save = true;
-
-            // completely remove list if it is now empty
-            if list.is_empty() {
-                self.watch_lists.remove(user);
-            }
-        }
-    }
-
-    pub fn get_watch_list(&self, user: &serenity::User) -> Option<&HashSet<Escalator>> {
-        self.watch_lists.get(user)
+    pub fn get_watch_list(&self, user: &serenity::User) -> Option<WatchList> {
+        self.watch_lists.get(user).copied()
     }
 
     pub async fn alert(
@@ -92,6 +98,10 @@ impl Alerts {
         escalators: EscalatorInput,
         status: Status,
     ) {
+        let Ok(users) = self.users_watching(escalators) else {
+            return
+        };
+
         let emoji = status.emoji();
         let noun = escalators.message_noun();
         let is_are = if escalators.is_singular() {
@@ -102,26 +112,63 @@ impl Alerts {
         let status = status.as_id_str();
         let message = format!("`{emoji}` {noun} {is_are} `{status}`");
 
-        for user in self.users_watching(escalators) {
+        for user in users {
             user.direct_message(&cache_http, |msg| msg.content(&message))
                 .await
                 .ok();
         }
     }
 
-    fn users_watching(&self, escalators: EscalatorInput) -> impl Iterator<Item = &serenity::User> {
-        self.watch_lists
+    fn users_watching(
+        &self,
+        escalators: EscalatorInput,
+    ) -> Result<impl Iterator<Item = &serenity::User>, UnknownEscalatorError> {
+        let escalators = EscalatorIndex::try_from(escalators)?;
+
+        Ok(self
+            .watch_lists
             .iter()
             .filter_map(move |(user, watch_list)| {
-                is_on_watchlist(watch_list, escalators).then_some(user)
-            })
+                is_on_watch_list(watch_list, escalators).then_some(user)
+            }))
     }
 }
 
-fn is_on_watchlist(watch_list: &HashSet<Escalator>, escalators: EscalatorInput) -> bool {
+#[derive(Clone, Copy)]
+enum EscalatorIndex {
+    All,
+    Direct(usize),
+    Pair(usize, usize),
+}
+
+fn escalator_index(escalator: Escalator) -> Option<usize> {
+    ESCALATORS.into_iter().position(|e| e == escalator)
+}
+
+struct UnknownEscalatorError;
+
+impl TryFrom<EscalatorInput> for EscalatorIndex {
+    type Error = UnknownEscalatorError;
+
+    fn try_from(value: EscalatorInput) -> Result<Self, Self::Error> {
+        match value {
+            EscalatorInput::All => Ok(Self::All),
+            EscalatorInput::Direct(start, end) => escalator_index((start, end))
+                .ok_or(UnknownEscalatorError)
+                .map(Self::Direct),
+            EscalatorInput::Pair(a, b) => {
+                let forward = escalator_index((a, b)).ok_or(UnknownEscalatorError)?;
+                let reverse = escalator_index((b, a)).ok_or(UnknownEscalatorError)?;
+                Ok(Self::Pair(forward, reverse))
+            }
+        }
+    }
+}
+
+fn is_on_watch_list(watch_list: &WatchList, escalators: EscalatorIndex) -> bool {
     match escalators {
-        EscalatorInput::All => !watch_list.is_empty(),
-        EscalatorInput::Direct(start, end) => watch_list.contains(&(start, end)),
-        EscalatorInput::Pair(a, b) => watch_list.contains(&(a, b)) || watch_list.contains(&(b, a)),
+        EscalatorIndex::All => !watch_list.is_empty(),
+        EscalatorIndex::Direct(index) => watch_list[index],
+        EscalatorIndex::Pair(forward, reverse) => watch_list[forward] || watch_list[reverse],
     }
 }
