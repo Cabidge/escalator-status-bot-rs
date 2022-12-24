46a9e1e23b70ce8026e1605aace0e85897a2b48d
Cabidge <cabbageboi69@gmail.com>
2022-12-24 17:03:08 -0500

    ~reword register and kill commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -17,14 +17,14 @@ pub fn commands() -> Vec<poise::Command<crate::Data, Error>> {
     ]
 }
 
-/// Spawns a button panel to register application commands (dev-only).
+/// (dev-only) Spawn a button panel to register application commands.
 #[poise::command(prefix_command, owners_only)]
 async fn register(ctx: Context<'_>) -> Result<(), Error> {
     poise::builtins::register_application_commands_buttons(ctx).await?;
     Ok(())
 }
 
-/// Kills the bot (dev-only).
+/// (dev-only) Force quit the bot.
 #[poise::command(prefix_command, owners_only)]
 async fn kill(ctx: Context<'_>) -> Result<(), Error> {
     ctx.data().shard_manager.lock().await.shutdown_all().await;
