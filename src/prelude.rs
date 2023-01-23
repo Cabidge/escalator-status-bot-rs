4f3cf95dc6609019d6bf8b757016deeceda748cd
Cabidge <cabbageboi69@gmail.com>
2023-01-23 14:03:11 -0500

    +added Escalator to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,7 +2,7 @@ pub use poise::serenity_prelude as serenity;
 
 pub use crate::data::Data;
 
-pub use crate::data::escalator::EscalatorFloors;
+pub use crate::data::escalator::{EscalatorFloors, Escalator};
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
