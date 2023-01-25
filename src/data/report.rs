def9eb43d616908a2dd527c68207744fa1d197f8
Cabidge <cabbageboi69@gmail.com>
2023-01-25 11:53:37 -0500

    +impl Display for UserReport

--- src/data/report.rs
+++ src/data/report.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 use super::{escalator_input::EscalatorInput, status::Status};
 
 use smallvec::SmallVec;
+use std::fmt::Display;
 
 #[derive(Clone)]
 pub struct UserReport {
@@ -10,4 +11,20 @@ pub struct UserReport {
     escalators: EscalatorInput,
     affected_escalators: SmallVec<[EscalatorFloors; 2]>,
     new_status: Status,
+}
+
+impl Display for UserReport {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let emoji = self.new_status.emoji();
+        let reporter = self
+            .reporter
+            .map(|id| format!("<@{}>", id))
+            .unwrap_or_else(|| String::from("an unknown user"));
+
+        write!(
+            f,
+            "`{emoji}` {reporter} reported {}.",
+            self.escalators.message_noun()
+        )
+    }
 }
