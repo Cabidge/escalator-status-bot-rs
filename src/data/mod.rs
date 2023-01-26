5540780eb9b10c704af431a53578a4dc0ef22861
Cabidge <cabbageboi69@gmail.com>
2023-01-25 21:29:24 -0500

    ~changed Mutex to RwLock for channels

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -13,7 +13,7 @@ use tokio::sync::{broadcast, Mutex};
 pub struct Data {
     pub shard_manager: Arc<Mutex<serenity::ShardManager>>,
     pub pool: sqlx::PgPool,
-    channels: parking_lot::Mutex<channels::AnyChannels>,
+    channels: parking_lot::RwLock<channels::AnyChannels>,
 }
 
 impl Data {
@@ -21,20 +21,20 @@ impl Data {
         Self {
             shard_manager,
             pool,
-            channels: parking_lot::Mutex::new(channels::AnyChannels::new()),
+            channels: parking_lot::RwLock::new(channels::AnyChannels::new()),
         }
     }
 
     /// Attempts to send a value, not creating a Sender if no Receivers exist.
     pub fn send_message<T: 'static + Clone + Send + Sync>(&self, value: T) {
-        let _ = self.channels.lock().try_send(value).ok();
+        let _ = self.channels.read().try_send(value).ok();
     }
 
     pub fn sender<T: 'static + Clone + Send + Sync>(&self) -> broadcast::Sender<T> {
-        self.channels.lock().sender()
+        self.channels.write().sender()
     }
 
     pub fn receiver<T: 'static + Clone + Send + Sync>(&self) -> broadcast::Receiver<T> {
-        self.channels.lock().receiver()
+        self.channels.write().receiver()
     }
 }
