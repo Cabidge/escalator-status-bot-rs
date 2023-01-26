d11b4ba7e2b39a9d3df68b7df8f2ab6a620f1a91
Cabidge <cabbageboi69@gmail.com>
2023-01-25 19:40:34 -0500

    -removed is_valid_escalator function

--- src/prelude.rs
+++ src/prelude.rs
@@ -14,11 +14,3 @@ pub fn replace_builder_with<T>(builder: T) -> impl FnOnce(&mut T) -> &mut T {
         old_builder
     }
 }
-
-pub fn is_valid_escalator(start: u8, end: u8) -> bool {
-    // if the start and end are valid floors...
-    ((2..=9).contains(&start) && (2..=9).contains(&end))
-        // ...and the distance between the two floors is 2
-        //                           ...or it is the 2-3 or 3-2
-        && (start.abs_diff(end) == 2 || matches!((start, end), (2, 3) | (3, 2)))
-}
